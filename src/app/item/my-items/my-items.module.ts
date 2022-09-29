b6f9b2a0a3a7d32868534e2dd4ec7e8d62084c94
Isabela Ribeiro <belamribeirop@gmail.com>
2022-09-29 18:57:47 -0300

    remove unused file

--- src/app/item/my-items/my-items.module.ts
+++ src/app/item/my-items/my-items.module.ts
@@ -4,18 +4,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MyItemsPage } from './my-items.page';
-import { ExploreContainerComponentModule } from '../../explore-container/explore-container.module';
-
 import { MyItemsPageRoutingModule } from './my-items-routing.module';
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    ExploreContainerComponentModule,
-    MyItemsPageRoutingModule,
-  ],
+  imports: [IonicModule, CommonModule, FormsModule, MyItemsPageRoutingModule],
   declarations: [MyItemsPage],
 })
 export class MyItemsPageModule {}
